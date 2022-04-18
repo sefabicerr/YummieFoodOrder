fc8ae59a73b6ce36507d321657f9eaadc5289b6e
Muhammed Sefa Bicer <sefaabicerr@gmail.com>
2022-04-19 01:39:56 +0300

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Muhammed Sefa Biçer on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
